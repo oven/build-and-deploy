8ad019fc8de4a5998792c42d11fc5a2c3809ebea
Ove Gram Nipen <ove@nipen.no>
2023-10-29 21:16:00 +0100

    step 3

--- code/src/main.js
+++ code/src/main.js
@@ -2,6 +2,7 @@ import {  getSecondsLeftOfYear, getTimeString } from "./clock";
 
 const timeContainer = document.getElementById("time");
 const timeLeft = document.getElementById("seconds-left");
+const unused = 1;
 
 const render = () => {
   const now = new Date();
